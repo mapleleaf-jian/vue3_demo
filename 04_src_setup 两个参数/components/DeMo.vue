db01b14991c29cd453604a8fa926efe9c7605176
mapleleaf-jian <1028517695@qq.com>
2022-07-27 22:48:14 +0800

    setup两个参数

---
+++ 04_src_setup 两个参数/components/DeMo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>hello</h2>
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+export default {
+  name: 'DeMo',
+  props: ['name', 'msg'],
+  setup(props, context) {
+    console.log('---', props) // Proxy {name: 'vue3', msg: 'hello'}
+    console.log(context)
+    const person = reactive({
+      name: '张三',
+      age: 18
+    })
+    return {
+      person
+    }
+  }
+}
+</script>
