23657ea0695eeaff38f12df7f59b7e1de646054a
mapleleaf-jian <1028517695@qq.com>
2022-07-30 19:58:49 +0800

    provide 与 inject

---
+++ 16_src_provide 与 inject/components/Son.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="son">
+    <span>孙组件</span>
+    <span>姓名：{{name}}</span>
+    <span> 年龄：{{age}}</span>
+  </div>
+</template>
+
+<script>
+import {inject, toRefs} from "vue";
+
+export default {
+  name: 'Son',
+  setup() {
+    let person = inject('person')
+    return {...toRefs(person)}
+  }
+}
+</script>
+
+<style scoped>
+  #son {
+    background-color: orange;
+    padding: 10px;
+  }
+</style>e
---
+++ src/components/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="child">
+    <span>子组件</span>
+    <Son/>
+  </div>
+</template>
+
+<script>
+import Son from './Son'
+export default {
+  name: 'Child',
+  components: {Son}
+}
+</script>
+
+<style scoped>
+  #child {
+    background-color: skyblue;
+    padding: 10px;
+  }
+</style>
